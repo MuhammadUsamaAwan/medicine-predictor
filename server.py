e1ae2587e222c88a96e1d60c7e00a77cb9edaedc
usama <underusama@gmail.com>
2022-11-23 10:52:54 +0500

    add trendwise graph

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ plt.style.use('fivethirtyeight')
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib
+from pylab import rcParams
 matplotlib.rcParams['axes.labelsize'] = 14
 matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
@@ -15,38 +16,43 @@ app = Flask(__name__)
 def home():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href="static/test.svg")
+        return render_template('index.html', trend="static/trendwise.svg", forcast="static/forcast.svg")
     else:
         medicine = request.form['medicine']
         steps = int(request.form['steps'])
         predict(medicine, steps)
         print('post')
-        return render_template('index.html', href="static/test.svg")
+        return render_template('index.html', trend="static/trendwise.svg", forcast="static/forcast.svg")
 
 
 def predict(medicine, step):
-    data = pd.read_csv("./data.csv",parse_dates=['TRANSDATE'])
+    data = pd.read_csv("./data.csv",parse_dates=['PERIOD'])
     value = data.loc[(data['PRIDESCR'] == medicine)]
     columns = ["NOMNCLTRE", "PRIDESCR", "STRENGTH", "TYPE", "DESCR", "AVGPRICE", "UNITPRICE", "UNIT"]
     value.drop(columns, inplace=True, axis=1)
-    value = value.sort_values('TRANSDATE')
-    value = value.groupby('TRANSDATE')['QUANTITY'].sum().reset_index()
-    value = value.set_index(value['TRANSDATE'])
-    y = value['QUANTITY'].resample('M').mean()
+    value = value.sort_values('PERIOD')
+    value = value.groupby('PERIOD')['CONSUMPTION QUANTITY'].sum().reset_index()
+    value = value.set_index(value['PERIOD'])
+    y = value['CONSUMPTION QUANTITY'].resample('M').mean()
+    rcParams['figure.figsize'] = 12, 8
+    decomposition = sm.tsa.seasonal_decompose(value['CONSUMPTION QUANTITY'], model='multiplicative', period=12, extrapolate_trend=12)
+    decomposition.plot()
+    plt.savefig("./static/trendwise.svg")
+    plt.switch_backend('agg')
     mod = sm.tsa.statespace.SARIMAX(y, order=(1, 1, 1), seasonal_order=(1, 1, 0, 12), enforce_invertibility=False)
     results = mod.fit()
     pred_uc = results.get_forecast(steps=step)
     pred_ci = pred_uc.conf_int()
-    ax = y.plot(label='observed', figsize=(18, 14))
+    ax = y.plot(label='Actual', figsize=(18, 14))
     pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
     ax.fill_between(pred_ci.index, pred_ci.iloc[:, 0], pred_ci.iloc[:, 1], color='k', alpha=.25)
-    ax.set_xlabel('TRANSDATE')
-    ax.set_ylabel('QUANTITY')
+    ax.set_xlabel('PERIOD')
+    ax.set_ylabel('CONSUMPTION QUANTITY')
     plt.title("Predicted Graph for "+medicine)
     plt.legend()
-    plt.savefig("./static/test.svg")
+    plt.savefig("./static/forcast.svg")
     plt.switch_backend('agg')
     return 'saved'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
